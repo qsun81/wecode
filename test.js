128a3eafedae2681341c9180067b28f4a694909e
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-12 10:47:22 +0900

    ADD: test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const findEvenNumbe r=  () =>{
+	let oneToFifty = 1;
+	const evenNumber = [];
+	for(oneToFifty ; oneToFifty < 51;oneToFifty++){
+	if(oneToFifty%2 === 0)
+		evenNumber.push(oneToFifty);
+ }
+	return evenNumber;
+ }
